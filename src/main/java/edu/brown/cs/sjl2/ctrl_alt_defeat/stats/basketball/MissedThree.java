ec7f9cda6de9847dd8bedead696f62c02f333266
Stewart Lynch <stewart_lynch@brown.edu>
2015-04-01 22:57:12 -0400

    some more stuff

---
+++ src/main/java/edu/brown/cs/sjl2/ctrl_alt_defeat/stats/basketball/MissedThree.java
@@ -0,0 +1,34 @@
+package edu.brown.cs.sjl2.ctrl_alt_defeat.stats.basketball;
+
+import edu.brown.cs.sjl2.ctrl_alt_defeat.Position;
+import edu.brown.cs.sjl2.ctrl_alt_defeat.basketball.Player;
+import edu.brown.cs.sjl2.ctrl_alt_defeat.stats.PlayerStats;
+import edu.brown.cs.sjl2.ctrl_alt_defeat.stats.Stat;
+
+public class MissedThree implements Stat {
+
+  @Override
+  public void execute(PlayerStats ps) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void undo(PlayerStats ps) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public Position getPosition() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Player getPlayer() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ src/main/java/edu/brown/cs/sjl2/ctrl_alt_defeat/stats/basketball/MissedTwo.java
@@ -0,0 +1,7 @@
+package edu.brown.cs.sjl2.ctrl_alt_defeat.stats.basketball;
+
+import edu.brown.cs.sjl2.ctrl_alt_defeat.stats.Stat;
+
+public interface MissedTwo extends Stat {
+
+}
