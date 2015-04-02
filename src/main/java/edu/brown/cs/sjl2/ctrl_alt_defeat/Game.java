f65d28c1fd691a6cb17253ce05cb277a8e3eaf3e
Nick Goelz <nicholas_goelz@brown.edu>
2015-04-01 22:49:34 -0400

    more classes and fleshed a couple out

--- src/main/java/edu/brown/cs/sjl2/ctrl_alt_defeat/Game.java
+++ src/main/java/edu/brown/cs/sjl2/ctrl_alt_defeat/Game.java
@@ -1,5 +1,37 @@
 package edu.brown.cs.sjl2.ctrl_alt_defeat;
 
-public class Game {
+import java.util.List;
 
+public class Game {
+  private Team homeTeam;
+  private Team awayTeam;
+  private Lineup homeLineup;
+  private Lineup awayLineup;
+  private Bench homeBench;
+  private Bench awayBench;
+  private Scoreboard scoreboard;
+  private BoxScore homeBoxScore;
+  private BoxScore awayBoxScore;
+  private List<Stat> stats;
+  
+  public Player getPlayerAtPosition(Position pos) {return null;}
+  public BoxScore getHomeBoxScore() {
+    return homeBoxScore;
+  }
+  public BoxScore getAwayBoxScore() {
+    return awayBoxScore;
+  }
+  private Scoreboard createScoreBoard() {
+    return null;
+  }
+  public void subPlayer(int idIn, int idOut) {}
+  public void takeTimeout(Boolean home) {}
+  public void flipPossession() {
+    scoreboard.flipPossession();
+  }
+  public void addStat(Stat s) {}
+  public List<Player> getTopPlayers(int n) {
+    return null;
+  }
+  
 }
--- src/main/java/edu/brown/cs/sjl2/ctrl_alt_defeat/Scoreboard.java
+++ src/main/java/edu/brown/cs/sjl2/ctrl_alt_defeat/Scoreboard.java
@@ -1,5 +1,64 @@
 package edu.brown.cs.sjl2.ctrl_alt_defeat;
 
 public class Scoreboard {
+  private int milliseconds;
+  private int seconds;
+  private int minutes;
+  private int quarter;
+  private int homeScore;
+  private int awayScore;
+  private int homeTO;
+  private int awayTO;
+  private boolean possession;
+  private int homeFouls;
+  private int awayFouls;
+  
+  public int getMS() {
+    return milliseconds;
+  }
+  
+  public int getSeconds() {
+    return seconds;
+  }
+  
+  public int getMinutes() {
+    return minutes;
+  }
+  
+  public int getQuarter() {
+    return quarter;
+  }
+  
+  public int getHomeScore() {
+    return homeScore;
+  }
+  
+  public int getAwayScore() {
+    return awayScore;
+  }
+  
+  public int getHomeTO() {
+    return homeTO;
+  }
+  
+  public int getAwayTO() {
+    return awayTO;
+  }
+  
+  public int getHomeFouls() {
+    return homeFouls;
+  }
+  
+  public int getAwayFouls() {
+    return awayFouls;
+  }
+
+  public boolean isPossession() {
+    return possession;
+  }
+
+  public void flipPossession() {
+    this.possession = !possession;
+  }
 
 }
