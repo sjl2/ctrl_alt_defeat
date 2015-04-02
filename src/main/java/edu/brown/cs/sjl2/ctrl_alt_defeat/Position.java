612693d953e8247f16a155c398d9e7fcf5f34e59
Stewart Lynch <stewart_lynch@brown.edu>
2015-04-01 22:59:51 -0400

    new position class

---
+++ src/main/java/edu/brown/cs/sjl2/ctrl_alt_defeat/Position.java
@@ -0,0 +1,19 @@
+package edu.brown.cs.sjl2.ctrl_alt_defeat;
+
+public class Position {
+  private double x;
+  private double y;
+
+  Position(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+}
