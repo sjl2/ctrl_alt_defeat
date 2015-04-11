0459c112f626d7ad1188d5e395b09013f5558a7f
Nick Goelz <nickgoelz@gmail.com>
2015-04-10 20:17:47 -0400

    and a little better

--- src/main/resources/static/js/nick.js
+++ src/main/resources/static/js/nick.js
@@ -2,6 +2,7 @@ var paper = Raphael(document.getElementById("content"), 500, 500);
 var bBox1 = paper.rect(100, 100, 120, 50, 10).attr({fill: 'darkorange', stroke: 'black', 'stroke-width': 2});
 var text1 = paper.text(bBox1.attrs.x + bBox1.attrs.width / 2, bBox1.attrs.y + bBox1.attrs.height / 2, 'Stat')
 .attr({"font-family": "Arial", "font-size": 16});
+text1.box = bBox1;
 
 var button1 = paper.set();
 button1.push(bBox1);
@@ -10,6 +11,8 @@ button1.push(text1);
 var bBox2 = paper.rect(100, 155, 120, 50, 10).attr({fill: 'darkorange', stroke: 'black', 'stroke-width': 2});
 var text2 = paper.text(bBox2.attrs.x + bBox2.attrs.width / 2, bBox2.attrs.y + bBox2.attrs.height / 2, 'Stat')
 .attr({"font-family": "Arial", "font-size": 16});
+text2.box = bBox2;
+
 
 var button2 = paper.set();
 button2.push(bBox2);
@@ -18,6 +21,8 @@ button2.push(text2);
 var bBox3 = paper.rect(100, 210, 120, 50, 10).attr({fill: 'darkorange', stroke: 'black', 'stroke-width': 2});
 var text3 = paper.text(bBox3.attrs.x + bBox3.attrs.width / 2, bBox3.attrs.y + bBox3.attrs.height / 2, 'Stat')
 .attr({"font-family": "Arial", "font-size": 16});
+text3.box = bBox3;
+
 
 var button3 = paper.set();
 button3.push(bBox3);
@@ -26,6 +31,8 @@ button3.push(text3);
 var bBox4 = paper.rect(100, 265, 120, 50, 10).attr({fill: 'darkorange', stroke: 'black', 'stroke-width': 2});
 var text4 = paper.text(bBox4.attrs.x + bBox4.attrs.width / 2, bBox4.attrs.y + bBox4.attrs.height / 2, 'Stat')
 .attr({"font-family": "Arial", "font-size": 16});
+text4.box = bBox4;
+
 
 var button4 = paper.set();
 button4.push(bBox4);
@@ -37,11 +44,33 @@ buttons.push(button2);
 buttons.push(button3);
 buttons.push(button4);
 
-buttons.mousedown(function(e) {
+var text = paper.set();
+text.push(text1);
+text.push(text2);
+text.push(text3);
+text.push(text4);
+
+var bBoxes = paper.set();
+bBoxes.push(bBox1);
+bBoxes.push(bBox2);
+bBoxes.push(bBox3);
+bBoxes.push(bBox4);
+
+bBoxes.mousedown(function(e) {
 	this.attr({fill: '#a2591d'});
 });
-buttons.mouseup(function(e) {
+bBoxes.mouseup(function(e) {
 	this.attr({fill: 'darkorange'});
+});
+bBoxes.mouseout(function(e) {
+	this.attr({fill: 'darkorange'});
+});
+text.mousedown(function(e) {
+	this.box.attr({fill: '#a2591d'});
+});
+text.mouseup(function(e) {
+	this.box.attr({fill: 'darkorange'});
+
 });
 
 
