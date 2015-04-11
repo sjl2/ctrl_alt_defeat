36df53b2602e3d2e9582f556fc7934bf9a225901
Nick Goelz <nickgoelz@gmail.com>
2015-04-10 23:36:22 -0400

    bunch of buttons

--- src/main/resources/static/js/nick.js
+++ src/main/resources/static/js/nick.js
@@ -1,92 +1,121 @@
-var paper = Raphael(document.getElementById("content"), 500, 500);
-var bBox1 = paper.rect(100, 100, 120, 50, 10).attr({fill: 'darkorange', stroke: 'black', 'stroke-width': 2});
-var text1 = paper.text(bBox1.attrs.x + bBox1.attrs.width / 2, bBox1.attrs.y + bBox1.attrs.height / 2, 'Stat')
-.attr({"font-family": "Arial", "font-size": 16});
-text1.box = bBox1;
+var homeColor = 'darkorange';
+var homeClick = "#a2591d";
+var homeAccent = "blue";
 
-var button1 = paper.set();
-button1.push(bBox1);
-button1.push(text1);
+var awayColor = 'green';
+var awayClick = "#0a390c";
+var awayAccent = "red";
 
-var bBox2 = paper.rect(100, 155, 120, 50, 10).attr({fill: 'darkorange', stroke: 'black', 'stroke-width': 2});
-var text2 = paper.text(bBox2.attrs.x + bBox2.attrs.width / 2, bBox2.attrs.y + bBox2.attrs.height / 2, 'Stat')
-.attr({"font-family": "Arial", "font-size": 16});
-text2.box = bBox2;
+var positions = ["PG", "SG", "SF", "PF", "C"];
+var stats = [["Block", "Defensive\nFoul", "Defensive\nRebound", "Free\nThrow"],
+			["Missed\nFree\nThrow", "Missed\n3PG", "Missed\n2PG", "Offensive\nFoul"],
+			["Offensive\nRebound", "Steal", "Technical\nFoul", "Turnover"],
+			["3PG", "2PG", "blah", "blahbity\nblah"]];
 
+var paper = Raphael(document.getElementById("content"), 1000, 1000);
 
-var button2 = paper.set();
-button2.push(bBox2);
-button2.push(text2);
-
-var bBox3 = paper.rect(100, 210, 120, 50, 10).attr({fill: 'darkorange', stroke: 'black', 'stroke-width': 2});
-var text3 = paper.text(bBox3.attrs.x + bBox3.attrs.width / 2, bBox3.attrs.y + bBox3.attrs.height / 2, 'Stat')
-.attr({"font-family": "Arial", "font-size": 16});
-text3.box = bBox3;
-
-
-var button3 = paper.set();
-button3.push(bBox3);
-button3.push(text3);
-
-var bBox4 = paper.rect(100, 265, 120, 50, 10).attr({fill: 'darkorange', stroke: 'black', 'stroke-width': 2});
-var text4 = paper.text(bBox4.attrs.x + bBox4.attrs.width / 2, bBox4.attrs.y + bBox4.attrs.height / 2, 'Stat')
-.attr({"font-family": "Arial", "font-size": 16});
-text4.box = bBox4;
-
-
-var button4 = paper.set();
-button4.push(bBox4);
-button4.push(text4);
-
+var boxes = paper.set();
+var texts = paper.set();
 var buttons = paper.set();
-buttons.push(button1);
-buttons.push(button2);
-buttons.push(button3);
-buttons.push(button4);
 
-var text = paper.set();
-text.push(text1);
-text.push(text2);
-text.push(text3);
-text.push(text4);
+for (var i = 0; i < 5; i++) {
+	var tempBox = paper.rect(50, 100 + 55 * i, 120, 50, 10).attr({fill: homeColor, stroke: 'black', 'stroke-width': 2});
+	var tempText = paper.text(110, 100 + 55 * i + 25, positions[i]).attr({"font-family": "Arial", "font-size":16});
+	tempText.box = tempBox;
+	tempBox.glowColor = homeAccent;
+	tempBox.clickAccent = homeClick;
+	tempBox.normalColor = homeColor;
+	tempText.toFront();
 
-var bBoxes = paper.set();
-bBoxes.push(bBox1);
-bBoxes.push(bBox2);
-bBoxes.push(bBox3);
-bBoxes.push(bBox4);
+	texts.push(tempText);
+	boxes.push(tempBox);
+}
 
-bBoxes.mousedown(function(e) {
-	this.attr({fill: '#a2591d'});
+for (var i = 0; i < 5; i++) {
+	var tempBox = paper.rect(600, 100 + 55 * i, 120, 50, 10).attr({fill: awayColor, stroke: 'black', 'stroke-width': 2});
+	var tempText = paper.text(660, 100 + 55 * i + 25, positions[i]).attr({"font-family": "Arial", "font-size":16});
+	tempText.box = tempBox;
+	tempBox.glowColor = awayAccent;
+	tempBox.clickAccent = awayClick;
+	tempBox.normalColor = awayColor;
+	tempText.toFront();
+
+	texts.push(tempText);
+	boxes.push(tempBox);
+}
+
+for (var i = 0; i < 4; i++) {
+	for (var j = 0; j < 4; j++) {
+		var tempButton = paper.circle(170 + 70 + 90 * i, 100 + 90 * j, 35).attr({fill: '#8a8a89', "stroke-width" : 2});
+		var tempText = paper.text(170 + 70 + 90 * i, 100 + 90 * j, stats[i][j]);
+		tempText.box = tempButton;
+		tempButton.glowColor = "#646a00";
+		tempButton.clickAccent = "#a4a4a2";
+		tempButton.normalColor = "#8a8a89";
+		texts.push(tempText);
+		buttons.push(tempButton);
+	}
+}
+
+boxes.mousedown(function(e) {
+	this.attr({fill: this.clickAccent});
 });
-bBoxes.mouseup(function(e) {
-	this.attr({fill: 'darkorange'});
+boxes.mouseup(function(e) {
+	this.attr({fill: this.normalColor});
 });
-bBoxes.mouseout(function(e) {
-	this.attr({fill: 'darkorange'});
-});
-text.mousedown(function(e) {
-	this.box.attr({fill: '#a2591d'});
-});
-text.mouseup(function(e) {
-	this.box.attr({fill: 'darkorange'});
-
+boxes.mouseout(function(e) {
+	this.attr({fill: this.normalColor});
 });
 
+buttons.mousedown(function(e) {
+	this.attr({fill: this.clickAccent});
+});
+buttons.mouseup(function(e) {
+	this.attr({fill: this.normalColor});
+});
+buttons.mouseout(function(e) {
+	this.attr({fill: this.normalColor});
+});
+
+texts.mousedown(function(e) {
+	this.box.attr({fill: this.box.clickAccent});
+});
+texts.mouseup(function(e) {
+	this.box.attr({fill: this.box.normalColor});
+});
 
 
 
-buttons.mouseover(function (event) {
+
+boxes.mouseover(function (event) {
     console.log("here");
     this.g = this.glow({
         opacity: 0.85,
-        color: 'blue',
+        color: this.glowColor,
         width: 15
     });
 }).mouseout(function (event) {
     this.g.remove();
 });
 
-buttons.click(function(e) {
-	console.log("clicked!");
+buttons.mouseover(function (event) {
+    console.log("here");
+    this.g = this.glow({
+        opacity: 0.85,
+        color: this.glowColor,
+        width: 15
+    });
+}).mouseout(function (event) {
+    this.g.remove();
 });
+
+texts.mouseover(function(e) {
+	this.box.g = this.box.glow({
+		opacity: 0.85,
+        color: this.box.glowColor,
+        width: 15
+	});
+}).mouseout(function (e) {
+	this.box.g.remove();
+});
+
