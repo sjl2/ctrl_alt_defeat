5f77a6b18a71ae2a1c2e81252cdd780d2b9166db
Nick Goelz <nickgoelz@gmail.com>
2015-04-10 20:04:59 -0400

    made buttons work

--- src/main/resources/static/js/nick.js
+++ src/main/resources/static/js/nick.js
@@ -1,5 +1,4 @@
 var paper = Raphael(document.getElementById("content"), 500, 500);
-
 var bBox1 = paper.rect(100, 100, 120, 50, 10).attr({fill: 'darkorange', stroke: 'black', 'stroke-width': 2});
 var text1 = paper.text(bBox1.attrs.x + bBox1.attrs.width / 2, bBox1.attrs.y + bBox1.attrs.height / 2, 'Stat')
 .attr({"font-family": "Arial", "font-size": 16});
@@ -21,22 +20,29 @@ var text3 = paper.text(bBox3.attrs.x + bBox3.attrs.width / 2, bBox3.attrs.y + bB
 .attr({"font-family": "Arial", "font-size": 16});
 
 var button3 = paper.set();
-button3.push(bBox1);
-button3.push(text1);
+button3.push(bBox3);
+button3.push(text3);
 
 var bBox4 = paper.rect(100, 265, 120, 50, 10).attr({fill: 'darkorange', stroke: 'black', 'stroke-width': 2});
 var text4 = paper.text(bBox4.attrs.x + bBox4.attrs.width / 2, bBox4.attrs.y + bBox4.attrs.height / 2, 'Stat')
 .attr({"font-family": "Arial", "font-size": 16});
 
 var button4 = paper.set();
-button4.push(bBox1);
-button4.push(text1);
+button4.push(bBox4);
+button4.push(text4);
 
 var buttons = paper.set();
 buttons.push(button1);
 buttons.push(button2);
 buttons.push(button3);
 buttons.push(button4);
+
+buttons.mousedown(function(e) {
+	this.attr({fill: '#a2591d'});
+});
+buttons.mouseup(function(e) {
+	this.attr({fill: 'darkorange'});
+});
 
 
 
