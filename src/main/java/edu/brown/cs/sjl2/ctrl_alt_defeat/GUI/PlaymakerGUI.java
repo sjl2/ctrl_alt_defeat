6d7e87c37f4a45fedec327d279396f83cb47ae17
Alex Wainger <alex_wainger@brown.edu>
2015-04-15 22:25:01 -0400

    GUI Classes for handlers

---
+++ src/main/java/edu/brown/cs/sjl2/ctrl_alt_defeat/GUI/PlaymakerGUI.java
@@ -0,0 +1,17 @@
+package edu.brown.cs.sjl2.ctrl_alt_defeat.GUI;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class PlaymakerGUI {
+
+  public static class SaveHandler implements Route {
+
+    @Override
+    public Object handle(Request request, Response response) {
+      
+      return null;
+    }
+  }
+}
