d690812230fa3d616ccc74d83d3c995509cfc16b
Nick Goelz <nicholas_goelz@brown.edu>
2015-04-17 01:04:19 -0400

    fully working(ish) stats front end

--- src/main/java/edu/brown/cs/sjl2/ctrl_alt_defeat/GUI/StatsEntryGUI.java
+++ src/main/java/edu/brown/cs/sjl2/ctrl_alt_defeat/GUI/StatsEntryGUI.java
@@ -4,10 +4,12 @@ import java.util.Map;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 import edu.brown.cs.sjl2.ctrl_alt_defeat.Dashboard;
 import edu.brown.cs.sjl2.ctrl_alt_defeat.GameException;
 import edu.brown.cs.sjl2.ctrl_alt_defeat.Location;
+import edu.brown.cs.sjl2.ctrl_alt_defeat.basketball.ScoreboardException;
 import spark.ModelAndView;
 import spark.QueryParamsMap;
 import spark.Request;
@@ -31,11 +33,11 @@ public class StatsEntryGUI {
       Map<String, Object> variables =
           ImmutableMap.of("tabTitle", "Stats Entry");
 
-//      if (dash.getGame() != null) {
-        return new ModelAndView(variables, "stats_entry.ftl");
-//      } else {
-//        return new ModelAndView(variables, "no_game.ftl");
-//      }
+      //      if (dash.getGame() != null) {
+      return new ModelAndView(variables, "stats_entry.ftl");
+      //      } else {
+      //        return new ModelAndView(variables, "no_game.ftl");
+      //      }
 
     }
 
@@ -47,7 +49,7 @@ public class StatsEntryGUI {
     public Object handle(Request request, Response response) {
       QueryParamsMap qm = request.queryMap();
       String statID = qm.value("statID");
-      int playerID = 1;
+      int playerID = GSON.fromJson(qm.value("playerID"), Integer.class);
       double x = GSON.fromJson(qm.value("x"), Double.class);
       double y = GSON.fromJson(qm.value("y"), Double.class);
       System.out.println(statID + " " + playerID + " " + x + " " + y);
@@ -63,5 +65,53 @@ public class StatsEntryGUI {
       return 28;
     }
   }
+
+  public class FlipPossessionHandler implements Route {
+
+    @Override
+    public Object handle(Request request, Response response) {
+      System.out.println("changed possession");
+      dash.getGame().flipPossession();
+
+      return 27;
+    }
+  }
+  
+  public class TimeoutHandler implements Route {
+
+    @Override
+    public Object handle(Request request, Response response) {
+      System.out.println("timeout " + request.queryMap().value("h"));
+      
+      try {
+        dash.getGame().takeTimeout(GSON.fromJson(request.queryMap().value("h"), Boolean.class));
+      } catch (JsonSyntaxException | GameException e) {
+        System.out.println("mistake handling timeout");
+      };
+
+      return 27;
+    }
+  }
+
+  public class SubHandler implements Route {
+    @Override
+    public Object handle(Request request, Response response) {
+      QueryParamsMap qm = request.queryMap();
+      int inPlayerID = GSON.fromJson(qm.value("in"), Integer.class);
+      int outPlayerID = GSON.fromJson(qm.value("out"), Integer.class);
+      boolean home = Boolean.parseBoolean(qm.value("home"));
+
+      try {
+        dash.getGame().subPlayer(inPlayerID, outPlayerID, home);
+      } catch (ScoreboardException e) {
+        System.out.println("there was an error subbing the players");
+      } catch (Exception e) {
+        System.out.println("the error is not well classified");
+      }
+      return 25;
+    }
+
+  }
+
 
 }
