ff323dee7f8596af3fb36bc10a568317cf9b090a
Nick Goelz <nicholas_goelz@brown.edu>
2015-04-17 02:03:07 -0400

    better whiteboard

--- src/main/resources/static/js/whiteboard.js
+++ src/main/resources/static/js/whiteboard.js
@@ -8,13 +8,21 @@ image.onload = function() {
 }
 
 drag = false;
+var prevPoint;
+ctx.lineWidth = 3;
 
 canvas.addEventListener("mousedown", function(e){drag = true;});
-canvas.addEventListener("mouseup", function(e){drag = false;});
-canvas.addEventListener("mouseout", function(e){drag = false;});
+canvas.addEventListener("mouseup", function(e){prevPoint = undefined; drag = false;});
+canvas.addEventListener("mouseout", function(e){prevPoint = undefined; drag = false;});
 canvas.addEventListener("mousemove", function(e){
 	if (drag) {
-		ctx.fillStyle = "rgb(" + Math.floor(Math.random() * 255) +"," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
-		ctx.fillRect(e.offsetX - 2, e.offsetY - 2, 4, 4);
+		ctx.strokeStyle = "rgb(" + Math.floor(Math.random() * 255) +"," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+		if ((prevPoint === undefined)) ctx.moveTo(e.offsetX, e.offsetY);
+			ctx.lineTo(e.offsetX, e.offsetY);
+			ctx.stroke();
+			prevPoint = {};
+			prevPoint.x = e.offsetX;
+			prevPoint.y = e.offsetY;
+		//ctx.fillRect(e.offsetX - 2, e.offsetY - 2, 4, 4);
 	}
 });
